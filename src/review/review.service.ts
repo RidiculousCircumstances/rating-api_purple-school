cf8bf29121930dc637b0f502ac7d36570ace377e
Ivan Kozlov <103379782+RidiculousCircumstances@users.noreply.github.com>
2022-08-08 12:50:12 +0700

    git actions

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -18,6 +18,7 @@ export class ReviewService {
 	}
 
 	async getByProductId(productId: string): Promise<DocumentType<ReviewModel>[]> {
+		console.log('You have connected via local network!');
 		return this.reviewModel.find({ productId: new Types.ObjectId(productId) }).exec();
 	}
 
