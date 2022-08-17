16ff1a88d806508a4d38653788a4f9d52286ef56
Ivan Kozlov <103379782+RidiculousCircumstances@users.noreply.github.com>
2022-08-18 01:11:44 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
+// async function bootstrap(): Promise<void> {
+// 	const app = await NestFactory.create(AppModule);
+// 	app.setGlobalPrefix('api');
+// 	const configService: ConfigService = app.get(ConfigService);
+// 	const port = +configService.get('PORT');
+// 	await app.listen(port);
+// }
+
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('api');
-	const configService: ConfigService = app.get(ConfigService);
-	const port = +configService.get('PORT');
-	await app.listen(port);
+	await app.listen(3000);
 }
 bootstrap();
