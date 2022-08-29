53d30f96d2835405f1db66aa760721cd987ceef0
Ivan Kozlov <103379782+RidiculousCircumstances@users.noreply.github.com>
2022-08-29 13:35:29 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('api');
-	app.enableCors({ origin: 'http://83.246.208.128:3000/' });
 	const configService: ConfigService = app.get(ConfigService);
 	const port = +configService.get('PORT');
+	app.enableCors({ origin: configService.get('PRODUCTION_ORIGIN') });
+	//app.enableCors({ origin: configService.get('DEV_ORIGIN') });
 	await app.listen(port);
 }
 
